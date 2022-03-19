f12bb08a4c849cbbb6c3459811e4c6ae748f3090
Papasha <Pavelevikin@yandex.ru>
2022-03-20 00:04:09 +0300

    Понял что проблема с количеством запросов...

--- bot.py
+++ bot.py
@@ -4,26 +4,27 @@ import seaborn as sns
 from binance.spot import Spot as Client
 
 listCoin = open('listCoin')
-print(listCoin.readline())
 
-# spot_client = Client(base_url="https://api3.binance.com")
-# depth_dict = spot_client.depth("ETHUSDT", limit=10000)
+for row in listCoin:
 
-# dBids = []
-# dAsks = []
-# price = 500000
+    spot_client = Client(base_url="https://api3.binance.com")
+    depth_dict = spot_client.depth("1INCHUSDT", limit=10000)
 
-# for bid in depth_dict['bids']:
-#     bid.append(float(bid[0])*float(bid[1]))
-#     if bid[2]>price:
-#         dBids.append(bid)
-# print(f"BIDS: {dBids}")
+    dBids = []
+    dAsks = []
+    price = 500000
 
-# for ask in depth_dict['asks']:
-#     ask.append(float(ask[0])*float(ask[1]))
-#     if ask[2]>price:
-#         dAsks.append(ask)
-# print(f"ASKS: {dAsks}")
+    for bid in depth_dict['bids']:
+        bid.append(float(bid[0])*float(bid[1]))
+        if bid[2]>price:
+            dBids.append(bid)
+    print(f"BIDS: {dBids}")
+
+    for ask in depth_dict['asks']:
+        ask.append(float(ask[0])*float(ask[1]))
+        if ask[2]>price:
+            dAsks.append(ask)
+    print(f"ASKS: {dAsks}")
 
 
 # df_list = []
